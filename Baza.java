974abb04016dd942621356c725e38cf2fcdf0b65
Valeriesantos <valnika96@gmail.com>
2017-02-02 10:45:34 +0100

    Crear clase y constructor

---
+++ Baza.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Baza here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Baza
+{
+    private int numeroJugadoresATirarCarta;
+    private int paloQuePinta;
+  
+
+    /**
+     * Constructor for objects of class Baza
+     */
+    public Baza(int numeroJugadoresATirarCarta, int paloQuePinta)
+    {
+        this.numeroJugadoresATirarCarta = numeroJugadoresATirarCarta;
+        this.paloQuePinta = paloQuePinta;
+    }
+}
